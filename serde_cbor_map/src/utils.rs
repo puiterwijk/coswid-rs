0a8a75eb9c8a717c8f92a48bdd7fb258aedd022c
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-01-29 09:13:33 +0100

    Code reorganization
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

---
+++ serde_cbor_map/src/utils.rs
@@ -0,0 +1,15 @@
+pub(crate) fn list_to_tuple<A, B, C, D>(list: Vec<(A, B, C, D)>) -> (Vec<A>, Vec<B>, Vec<C>, Vec<D>) {
+    let mut a: Vec<A> = Vec::new();
+    let mut b: Vec<B> = Vec::new();
+    let mut c: Vec<C> = Vec::new();
+    let mut d: Vec<D> = Vec::new();
+
+    for (ia, ib, ic, id) in list {
+        a.push(ia);
+        b.push(ib);
+        c.push(ic);
+        d.push(id);
+    }
+
+    (a, b, c, d)
+}
