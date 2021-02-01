c10aa151de2e5af16c918364478a82d903df8f54
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-02-01 14:30:52 +0100

    Use re-exported derives from serde_int_map
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- src/parser.rs
+++ src/parser.rs
@@ -6,12 +6,18 @@ use std::fs::File;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!("Usage: $0 <filename>");
+    if args.len() < 2 {
+        println!("Usage: $0 <filename> [raw]");
         return Ok(());
     }
 
     let reader = File::open(&args[1]).unwrap();
+
+    if args.len() == 3 && args[2] == "raw" {
+        let raw_parsed: ciborium::value::Value = ciborium::de::from_reader(&reader)?;
+        println!("Raw parsed: {:?}", raw_parsed);
+        return Ok(());
+    }
     let parsed: CoSWIDTag = ciborium::de::from_reader(&reader)?;
     println!("Parsed CoSWID: {:?}", parsed);
 
